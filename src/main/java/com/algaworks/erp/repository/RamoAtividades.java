15e6f58195d005e9b9c0676a2ac26269a4b7748f
Ana Carolina Bilecki <anacarolina255@gmail.com>
2023-02-13 10:07:59 -0300

    Inserindo a anotação @Inject

--- src/main/java/com/algaworks/erp/repository/RamoAtividades.java
+++ src/main/java/com/algaworks/erp/repository/RamoAtividades.java
@@ -3,6 +3,8 @@ package com.algaworks.erp.repository;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import com.algaworks.erp.model.RamoAtividade;
 
 import jakarta.persistence.EntityManager;
@@ -11,6 +13,7 @@ import jakarta.persistence.TypedQuery;
 public class RamoAtividades implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Inject
 	private EntityManager manager;
 	
 	public RamoAtividades() {
